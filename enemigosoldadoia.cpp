dd242434b55e150f2ab945375ec2a68c682bdcad
KeinerTorres <keiner.torres1@udea.edu.co>
2025-12-06 11:36:16 -0500

    Movimientos enemigo IA

--- enemigosoldadoia.cpp
+++ enemigosoldadoia.cpp
@@ -34,3 +34,64 @@ void EnemigoSoldadoIA::establecerCuadrosEntreAtaques(int cuadros)
 }
 
 
+void EnemigoSoldadoIA::actualizarIA()
+{
+    if (estaEliminado() || !objetivo || objetivo->estaMuerta())
+    {
+        velocidadX = 0.0f;
+
+        actualizarMovimientoBasico();
+        return;
+    }
+
+    ejecutarMovimientoIA();
+    actualizarMovimientoBasico();
+    intentarAtacar();
+}
+
+void EnemigoSoldadoIA::ejecutarMovimientoIA()
+{
+    if (!objetivo)
+        return;
+
+    float dx = objetivo->obtenerPosicion().x() - obtenerPosicion().x();
+    float distancia = qAbs(dx);
+
+    if (distancia > distanciaDeteccion)
+    {
+        velocidadX = 0.0f;
+        return;
+    }
+
+    if (dx < -1)
+        velocidadX = -velocidadMovimientoIA;
+    else if (dx > 1)
+        velocidadX = velocidadMovimientoIA;
+    else
+        velocidadX = 0.0f;
+}
+
+void EnemigoSoldadoIA::intentarAtacar()
+{
+    if (!objetivo || estaEliminado() || objetivo->estaMuerta())
+        return;
+
+    if (contadorCuadrosAtaque > 0)
+    {
+        contadorCuadrosAtaque--;
+        return;
+    }
+
+    Hitbox *miHitbox = obtenerHitbox();
+    Hitbox *hbObjetivo = objetivo->obtenerHitbox();
+
+    if (!miHitbox || !hbObjetivo)
+        return;
+
+    if (miHitbox->colisionaCon(hbObjetivo))
+    {
+        atacarEntidad(objetivo);
+        contadorCuadrosAtaque = cuadrosEntreAtaques;
+        qDebug() << "EnemigoSoldadoIA atacó. Vida jugador:" << objetivo->obtenerVida();
+    }
+}
