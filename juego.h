4936a320b0b1d31b0a9fa544040ab823e0e2db70
KeinerTorres <keiner.torres1@udea.edu.co>
2025-12-08 20:04:02 -0500

    Interfaz grafica y gestor del juego

--- juego.h
+++ juego.h
@@ -2,22 +2,50 @@
 #define JUEGO_H
 
 #include <QMainWindow>
+#include <QPushButton>
+#include <QGraphicsView>
+#include <QLabel>
 
-QT_BEGIN_NAMESPACE
-namespace Ui {
-class juego;
-}
-QT_END_NAMESPACE
+#include "Nivel.h"
+#include "Nivel1.h"
+#include "Nivel3.h"
+#include "ui_juego.h"
 
 class juego : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    juego(QWidget *parent = nullptr);
+    explicit juego(QWidget *parent = nullptr);
     ~juego();
+
+    void establecerNivel(Nivel *nuevoNivel);
+    void iniciarJuego();
+    Nivel* obtenerNivelActual() const;
+    void cargarNivel(int numeroNivel);
 
 private:
     Ui::juego *ui;
+    QGraphicsView *vista;
+    Nivel *nivelActual;
+
+    int anchoVentana;
+    int altoVentana;
+
+    // Overlay de selección de país
+    QWidget *overlaySeleccion;
+    QPushButton *btnUcrania;
+    QPushButton *btnRusia;
+    QLabel *lblTituloSeleccion;
+    QLabel *lblUcrania;
+    QLabel *lblRusia;
+
+    bool jugadorEsUcrania;   // true = Ucrania, false = Rusia
+
+    void crearSeleccionBando();
+
+    void manejarNivelGanado(int numeroNivel);
+    void manejarNivelPerdido(int numeroNivel);
 };
+
 #endif // JUEGO_H
